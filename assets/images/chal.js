e953bcc4dd072126f1d31ca95d3d647e0003d819
fzafar83 <fzafar01@gmail.com>
2023-02-17 15:37:41 -0500

    modified html and readme.md

--- assets/images/chal.js
+++
@@ -1,5 +0,0 @@
-let firstVariable = 'Congratulations';
-let firstName = 'John Smith.';
-let message = 'You have just completed your first challenge.'
-
-console.log(firstVariable + firstName + message);
